650a2df29c989d4e06ac82008f1e49453580982c
jujuhere <mojomailservice@gmail.com>
2025-07-22 01:44:17 +0200

    Filter functions added for dataset

---
+++ data/filter_shapenet_classes.py
@@ -0,0 +1,48 @@
+import numpy as np
+import os
+
+
+target_classes = {
+    "chair": "03001627",
+    "plane": "02691156",
+    "sofa": "04256520",
+    "lamp": "03636649",
+    "table": "04379243"
+}
+max_per_class = 600  # amount per class
+
+
+samples_dict = np.load("samples_dict_ShapeNetCore.npy", allow_pickle=True).item()
+idx2str = np.load("idx_int2str_dict.npy", allow_pickle=True).item()
+str2idx = np.load("idx_str2int_dict.npy", allow_pickle=True).item()
+
+#new filter dictionaries
+filtered_samples_dict = {}
+filtered_idx2str = {}
+filtered_str2idx = {}
+
+new_index = 0
+
+
+for class_name, class_id in target_classes.items():
+    print(f"\n⏳ Filtering class: {class_name} ({class_id})")
+    
+    matching_ids = [
+        idx for idx, path in idx2str.items()
+        if path.startswith(class_id + "/")
+    ][:max_per_class]
+
+    for old_idx in matching_ids:
+        key = idx2str[old_idx]
+
+        # new index assign
+        filtered_samples_dict[new_index] = samples_dict[old_idx]
+        filtered_idx2str[new_index] = key
+        filtered_str2idx[key] = new_index
+        new_index += 1
+
+    
+np.save("samples_dict_classic.npy", filtered_samples_dict)
+np.save("idx_int2str_dict_classic.npy", filtered_idx2str)
+np.save("idx_str2int_dict_classic.npy", filtered_str2idx)
+
