cd398670cd323b41a4cb5f4ec436ac72bc4429e7
jujuhere <mojomailservice@gmail.com>
2025-07-23 01:59:09 +0200

    config changes -> sofa

--- data/filter_shapenet_classes.py
+++ data/filter_shapenet_classes.py
@@ -6,7 +6,7 @@ os.makedirs(results_dir, exist_ok=True)
 
 
 target_classes = {
-    "chair": "03001627",
+    "sofa": "04256520",
 }
 max_per_class = 600  # amount per class
 
@@ -43,7 +43,7 @@ for class_name, class_id in target_classes.items():
         new_index += 1
 
     
-np.save(os.path.join(results_dir, "samples_dict_chair.npy"), filtered_samples_dict)
+np.save(os.path.join(results_dir, "samples_dict_sofa.npy"), filtered_samples_dict)
 # np.save(os.path.join(results_dir, "idx_int2str_dict.npy"), filtered_idx2str)
 # np.save(os.path.join(results_dir, "idx_str2int_dict.npy"), filtered_str2idx)
 
