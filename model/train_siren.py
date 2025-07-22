08e4b47a5961f1ba9151819d7c91cd7c5bd31922
jujuhere <mojomailservice@gmail.com>
2025-07-22 11:34:43 +0200

    again fix

--- model/train_siren.py
+++ model/train_siren.py
@@ -53,9 +53,10 @@ class Trainer():
 
         # define optimisers
         self.optimizer_model = optim.Adam(self.model.parameters(), lr=self.train_cfg['lr_model'], weight_decay=0)
-        
+        data = dataset.SDFDataset(self.train_cfg['dataset'])
+        num_latent_classes = data.num_latent_classes
         # generate a unique random latent code for each shape
-        self.latent_codes = utils_deepsdf.generate_latent_codes(self.train_cfg['latent_size'], samples_dict, num_latent_classes=dataset.num_latent_classes)
+        self.latent_codes = utils_deepsdf.generate_latent_codes(self.train_cfg['latent_size'], samples_dict, num_latent_classes=num_latent_classes)
         self.optimizer_latent = optim.Adam([self.latent_codes], lr=self.train_cfg['lr_latent'], weight_decay=0)
         
         # Load pretrained weights and optimisers to continue training
