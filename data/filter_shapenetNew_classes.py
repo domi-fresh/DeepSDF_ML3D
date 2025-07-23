3b1c59441ac735c19cd206694a7a165f535dc7b9
jujuhere <mojomailservice@gmail.com>
2025-07-23 20:04:19 +0200

    filter guitar

--- data/filter_shapenetNew_classes.py
+++ data/filter_shapenetNew_classes.py
@@ -45,6 +45,6 @@ for class_name, class_id in target_classes.items():
         new_index += 1
 
     
-np.save(os.path.join(results_dir, "samples_dict_guitar.npy"), filtered_samples_dict)
-#np.save(os.path.join(results_dir, "idx_int2str_dict_new.npy"), filtered_idx2str)
-#np.save(os.path.join(results_dir, "idx_str2int_dict_new.npy"), filtered_str2idx)
+# np.save(os.path.join(results_dir, "samples_dict_guitar.npy"), filtered_samples_dict)
+np.save(os.path.join(results_dir, "idx_int2str_dict.npy"), filtered_idx2str)
+np.save(os.path.join(results_dir, "idx_str2int_dict.npy"), filtered_str2idx)
